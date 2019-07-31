d4b6779e61e07b098bc32be42731ae950840dd9b
Thomas Bleijendaal <thomasbleijendaal@outlook.com>
2019-07-31 12:41:14 +0200

    Some refactorings

--- RapidCMS/RapidCMS.Common/Data/Query.cs
+++ RapidCMS/RapidCMS.Common/Data/Query.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq.Expressions;
+﻿using System.Linq.Expressions;
 
 namespace RapidCMS.Common.Data
 {
@@ -54,47 +53,5 @@ namespace RapidCMS.Common.Data
         public string? SearchTerm { get; private set; }
 
         public bool MoreDataAvailable { get; private set; } = false;
-    }
-
-    internal class TypedQuery<TEntity> : IQuery, IQuery<TEntity>
-        where TEntity : IEntity
-    {
-        private readonly Query _query;
-
-        public static IQuery<TEntity> Convert(IQuery query)
-        {
-            if (query is Query q1)
-            {
-                return new TypedQuery<TEntity>(q1);
-            }
-            else if (query is TypedQuery<TEntity> q2)
-            {
-                return q2;
-            }
-            else
-            {
-                throw new InvalidOperationException();
-            }
-        }
-
-        public TypedQuery(Query query)
-        {
-            _query = query;
-        }
-
-        public int Skip => _query.Skip;
-
-        public int Take => _query.Take;
-
-        public string? SearchTerm => _query.SearchTerm;
-
-        public bool MoreDataAvailable => _query.MoreDataAvailable;
-
-        public Expression<Func<TEntity, bool>>? DataViewExpression => _query.QueryExpression as Expression<Func<TEntity, bool>>;
-
-        public void HasMoreData(bool hasMoreData)
-        {
-            _query.HasMoreData(hasMoreData);
-        }
     }
 }
---
+++ RapidCMS/RapidCMS.Common/Data/TypedQuery.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace RapidCMS.Common.Data
+{
+    internal class TypedQuery<TEntity> : IQuery, IQuery<TEntity>
+        where TEntity : IEntity
+    {
+        private readonly Query _query;
+
+        public static IQuery<TEntity> Convert(IQuery query)
+        {
+            if (query is Query q1)
+            {
+                return new TypedQuery<TEntity>(q1);
+            }
+            else if (query is TypedQuery<TEntity> q2)
+            {
+                return q2;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public TypedQuery(Query query)
+        {
+            _query = query;
+        }
+
+        public int Skip => _query.Skip;
+
+        public int Take => _query.Take;
+
+        public string? SearchTerm => _query.SearchTerm;
+
+        public bool MoreDataAvailable => _query.MoreDataAvailable;
+
+        public Expression<Func<TEntity, bool>>? DataViewExpression => _query.QueryExpression as Expression<Func<TEntity, bool>>;
+
+        public void HasMoreData(bool hasMoreData)
+        {
+            _query.HasMoreData(hasMoreData);
+        }
+    }
+}
