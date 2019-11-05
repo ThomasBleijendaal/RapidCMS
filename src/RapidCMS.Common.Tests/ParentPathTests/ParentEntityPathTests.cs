e80fb4453ef064e6c09b39034b6e726a420c6685
Thomas Bleijendaal <t.bleijendaal@wearetriple.com>
2019-11-05 12:25:01 +0100

    Minor bug fix

---
+++ src/RapidCMS.Common.Tests/ParentPathTests/ParentEntityPathTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using RapidCMS.Common.Data;
+
+namespace RapidCMS.Common.Tests.ParentPathTests
+{
+    public class ParentEntityPathTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void BasicPath()
+        {
+            // arrange
+            var parent = new ParentEntity(default, new Entity { Id = "1" }, "test1");
+
+            // act
+            var path = parent.GetParentPath();
+
+            // assert
+            Assert.AreEqual("test1:1", path.ToPathString());
+        }
+
+        [Test]
+        public void NestedPath()
+        {
+            // arrange
+            var parent = new ParentEntity(
+                new ParentEntity(default, new Entity { Id = "2" }, "test2"),
+                new Entity { Id = "1" }, "test1");
+
+            // act
+            var path = parent.GetParentPath();
+
+            // assert
+            Assert.AreEqual("test2:2;test1:1", path.ToPathString());
+        }
+
+        [Test]
+        public void DeepNestedPath()
+        {
+            // arrange
+            var parent = new ParentEntity(
+                new ParentEntity(
+                    new ParentEntity(default, new Entity { Id = "3" }, "test3"), 
+                    new Entity { Id = "2" }, "test2"),
+                new Entity { Id = "1" }, "test1");
+
+            // act
+            var path = parent.GetParentPath();
+
+            // assert
+            Assert.AreEqual("test3:3;test2:2;test1:1", path.ToPathString());
+        }
+
+        public class Entity : IEntity
+        {
+            public string Id { get; set; }
+        }
+    }
+}
---
+++ src/RapidCMS.Common.Tests/ParentPathTests/ParentPathTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using RapidCMS.Common.Data;
+
+namespace RapidCMS.Common.Tests.ParentPathTests
+{
+    public class ParentPathTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void BasicPath()
+        {
+            // arrange
+
+            // act
+            var path = ParentPath.TryParse("test:123");
+
+            // assert
+            Assert.AreEqual("test:123", path.ToPathString());
+        }
+
+        [Test]
+        public void AppendPath()
+        {
+            // arrange
+
+            // act
+            var path = ParentPath.TryParse("test:123");
+            var newPath = ParentPath.AddLevel(path, "test2", "1234");
+
+            // assert
+            Assert.AreEqual("test:123", path.ToPathString());
+            Assert.AreEqual("test:123;test2:1234", newPath.ToPathString());
+        }
+
+        [Test]
+        public void AppendPathFromNull()
+        {
+            // arrange
+
+            // act
+            var path = default(ParentPath);
+            var newPath = ParentPath.AddLevel(path, "test2", "1234");
+
+            // assert
+            Assert.AreEqual("test2:1234", newPath.ToPathString());
+        }
+    }
+}
