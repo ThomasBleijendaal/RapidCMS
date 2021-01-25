535a39bf95ff8417d2739f682aae3a0aa70ef0ef
Thomas Bleijendaal <t.bleijendaal@wearetriple.com>
2021-01-25 21:20:53 +0100

    Fixes #61 and #62

--- src/RapidCMS.Core/Repositories/ApiRepository.cs
+++ src/RapidCMS.Core/Repositories/ApiRepository.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using RapidCMS.Core.Abstractions.Data;
 using RapidCMS.Core.Abstractions.Forms;
@@ -25,26 +22,23 @@ namespace RapidCMS.Repositories.ApiBridge
         where TEntity : class, IEntity
         where TCorrespondingRepository : IRepository
     {
-        private readonly IHttpClientFactory _httpClientFactory;
-        private readonly IMemoryCache _memoryCache;
-        private readonly JsonSerializerSettings _jsonSerializerSettings;
-        private readonly string _repositoryAlias;
+        private readonly ApiRepositoryHelper _apiRepositoryHelper;
 
         public ApiRepository(IHttpClientFactory httpClientFactory, IMemoryCache memoryCache)
         {
-            _httpClientFactory = httpClientFactory;
-            _memoryCache = memoryCache;
-            _jsonSerializerSettings = new JsonSerializerSettings();
-            _jsonSerializerSettings.Converters.Add(new EntityModelJsonConverter<TEntity>());
-            _repositoryAlias = AliasHelper.GetRepositoryAlias(typeof(ApiRepository<TEntity, TCorrespondingRepository>));
+            var jsonSerializerSettings = new JsonSerializerSettings();
+            jsonSerializerSettings.Converters.Add(new EntityModelJsonConverter<TEntity>());
+            var repositoryAlias = AliasHelper.GetRepositoryAlias(typeof(ApiRepository<TEntity, TCorrespondingRepository>));
+
+            _apiRepositoryHelper = new ApiRepositoryHelper(memoryCache, httpClientFactory, jsonSerializerSettings, repositoryAlias);
         }
 
         public override Task DeleteAsync(string id, IParent? parent)
-            => DoRequestAsync(CreateRequest(HttpMethod.Delete, $"entity/{id}", new DeleteModel(parent)));
+            => _apiRepositoryHelper.DoRequestAsync(_apiRepositoryHelper.CreateRequest(HttpMethod.Delete, $"entity/{id}", new DeleteModel(parent)));
 
         public override async Task<IEnumerable<TEntity>> GetAllAsync(IParent? parent, IQuery<TEntity> query)
         {
-            var results = await DoRequestAsync<EntitiesModel<TEntity>>(CreateRequest(HttpMethod.Post, "all", new ParentQueryModel(parent, query)));
+            var results = await _apiRepositoryHelper.DoRequestAsync<EntitiesModel<TEntity>>(_apiRepositoryHelper.CreateRequest(HttpMethod.Post, "all", new ParentQueryModel(parent, query)));
             if (results == default)
             {
                 return Enumerable.Empty<TEntity>();
@@ -57,7 +51,7 @@ namespace RapidCMS.Repositories.ApiBridge
 
         public override async Task<IEnumerable<TEntity>?> GetAllRelatedAsync(IRelated related, IQuery<TEntity> query)
         {
-            var results = await DoRequestAsync<EntitiesModel<TEntity>>(CreateRequest(HttpMethod.Post, "all/related", new RelatedQueryModel(related, query)));
+            var results = await _apiRepositoryHelper.DoRequestAsync<EntitiesModel<TEntity>>(_apiRepositoryHelper.CreateRequest(HttpMethod.Post, "all/related", new RelatedQueryModel(related, query)));
             if (results == default)
             {
                 return Enumerable.Empty<TEntity>();
@@ -70,7 +64,7 @@ namespace RapidCMS.Repositories.ApiBridge
 
         public override async Task<IEnumerable<TEntity>?> GetAllNonRelatedAsync(IRelated related, IQuery<TEntity> query)
         {
-            var results = await DoRequestAsync<EntitiesModel<TEntity>>(CreateRequest(HttpMethod.Post, "all/nonrelated", new RelatedQueryModel(related, query)));
+            var results = await _apiRepositoryHelper.DoRequestAsync<EntitiesModel<TEntity>>(_apiRepositoryHelper.CreateRequest(HttpMethod.Post, "all/nonrelated", new RelatedQueryModel(related, query)));
             if (results == default)
             {
                 return Enumerable.Empty<TEntity>();
@@ -82,91 +76,24 @@ namespace RapidCMS.Repositories.ApiBridge
         }
 
         public override async Task<TEntity?> GetByIdAsync(string id, IParent? parent)
-            => (await DoRequestAsync<EntityModel<TEntity>>(CreateRequest(HttpMethod.Post, $"entity/{id}", new ParentQueryModel(parent))))?.Entity;
+            => (await _apiRepositoryHelper.DoRequestAsync<EntityModel<TEntity>>(_apiRepositoryHelper.CreateRequest(HttpMethod.Post, $"entity/{id}", new ParentQueryModel(parent))))?.Entity;
 
         public override async Task<TEntity?> InsertAsync(IEditContext<TEntity> editContext)
-            => (await DoRequestAsync<EntityModel<TEntity>>(CreateRequest(HttpMethod.Post, "entity", new EditContextModel<TEntity>(editContext))))?.Entity;
+            => (await _apiRepositoryHelper.DoRequestAsync<EntityModel<TEntity>>(_apiRepositoryHelper.CreateRequest(HttpMethod.Post, "entity", new EditContextModel<TEntity>(editContext))))?.Entity;
 
         public override async Task<TEntity> NewAsync(IParent? parent, Type? variantType = null)
-            => (await DoRequestAsync<EntityModel<TEntity>>(CreateRequest(HttpMethod.Post, "new", new ParentQueryModel(parent, variantType))))?.Entity ?? throw new NotFoundException("Could not create new entity.");
+            => (await _apiRepositoryHelper.DoRequestAsync<EntityModel<TEntity>>(_apiRepositoryHelper.CreateRequest(HttpMethod.Post, "new", new ParentQueryModel(parent, variantType))))?.Entity ?? throw new NotFoundException("Could not create new entity.");
 
         public override Task UpdateAsync(IEditContext<TEntity> editContext)
-            => DoRequestAsync(CreateRequest(HttpMethod.Put, $"entity/{editContext.Entity.Id}", new EditContextModel<TEntity>(editContext)));
+            => _apiRepositoryHelper.DoRequestAsync(_apiRepositoryHelper.CreateRequest(HttpMethod.Put, $"entity/{editContext.Entity.Id}", new EditContextModel<TEntity>(editContext)));
 
         public override Task AddAsync(IRelated related, string id)
-            => DoRequestAsync(CreateRequest(HttpMethod.Post, $"relate", new RelateModel(related, id)));
+            => _apiRepositoryHelper.DoRequestAsync(_apiRepositoryHelper.CreateRequest(HttpMethod.Post, $"relate", new RelateModel(related, id)));
 
         public override Task RemoveAsync(IRelated related, string id)
-            => DoRequestAsync(CreateRequest(HttpMethod.Delete, $"relate", new RelateModel(related, id)));
+            => _apiRepositoryHelper.DoRequestAsync(_apiRepositoryHelper.CreateRequest(HttpMethod.Delete, $"relate", new RelateModel(related, id)));
 
         public override Task ReorderAsync(string? beforeId, string id, IParent? parent)
-            => DoRequestAsync(CreateRequest(HttpMethod.Post, $"reorder", new ReorderModel(beforeId, id, parent)));
-
-        private HttpRequestMessage CreateRequest(HttpMethod method, string url)
-        {
-            return new HttpRequestMessage(method, url);
-        }
-
-        private HttpRequestMessage CreateRequest<T>(HttpMethod method, string url, T content)
-        {
-            if (method == HttpMethod.Get)
-            {
-                throw new InvalidOperationException();
-            }
-
-            var request = CreateRequest(method, url);
-            request.Content = new StringContent(JsonConvert.SerializeObject(content, _jsonSerializerSettings), Encoding.UTF8, "application/json");
-
-            return request;
-        }
-
-        private async Task<HttpResponseMessage> DoRequestAsync(HttpRequestMessage request)
-        {
-            var httpClient = _httpClientFactory.CreateClient(_repositoryAlias);
-            if (httpClient.BaseAddress == default)
-            {
-                throw new InvalidOperationException($"Please configure an HttpClient for the repository '{_repositoryAlias}' using " +
-                    $".{nameof(RapidCMSMiddleware.AddRapidCMSApiRepository)}([..]) and configure its BaseAddress correctly.");
-            }
-
-            var response = await httpClient.SendAsync(request);
-
-            return response.StatusCode switch
-            {
-                HttpStatusCode.OK => response,
-                HttpStatusCode.Unauthorized => throw new UnauthorizedAccessException(),
-                HttpStatusCode.Forbidden => throw new UnauthorizedAccessException(),
-                HttpStatusCode.NotFound => throw new NotFoundException($"{request.RequestUri} not found."),
-
-                _ => throw new InvalidOperationException()
-            };
-        }
-
-        private async Task<TResult?> DoRequestAsync<TResult>(HttpRequestMessage request)
-            where TResult : class
-        {
-            try
-            {
-                var cacheKey = $"{_repositoryAlias}-{request.RequestUri}";
-                if (_memoryCache.TryGetValue(cacheKey, out TResult cachedResult))
-                {
-                    return cachedResult;
-                }
-                var response = await DoRequestAsync(request);
-                var json = await response.Content.ReadAsStringAsync();
-                var result = JsonConvert.DeserializeObject<TResult>(json, _jsonSerializerSettings);
-
-                if (result != null)
-                {
-                    _memoryCache.Set(cacheKey, result, TimeSpan.FromSeconds(1));
-                }
-                
-                return result;
-            }
-            catch (NotFoundException)
-            {
-                return default;
-            }
-        }
+            => _apiRepositoryHelper.DoRequestAsync(_apiRepositoryHelper.CreateRequest(HttpMethod.Post, $"reorder", new ReorderModel(beforeId, id, parent)));        
     }
 }
