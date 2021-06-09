8168e482c49111d27f4d6ad63496606755262653
Thomas Bleijendaal <t.bleijendaal@wearetriple.com>
2021-06-09 20:11:12 +0200

    Source Generator pipeline running

---
+++ src/RapidCMS.ModelMaker.SourceGenerator.EFCore/Generators/EntityGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RapidCMS.ModelMaker.SourceGenerator.Abstractions;
+
+namespace RapidCMS.ModelMaker.SourceGenerator.EFCore.Generators
+{
+    public class EntityGenerator : IModelGenerator
+    {
+    }
+}
---
+++ src/RapidCMS.ModelMaker.SourceGenerator/ModelMakerGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using RapidCMS.ModelMaker.SourceGenerator.Abstractions;
+
+namespace RapidCMS.ModelMaker.SourceGenerator
+{
+    [Generator]
+    public class ModelMakerGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            //foreach (var file in context.AdditionalFiles)
+            //{
+            //    context.ReportDiagnostic(Diagnostic.Create("RC0001", "", "fdsa", DiagnosticSeverity.Error, DiagnosticSeverity.Error, true, 1));
+            //}
+
+            context.AddSource("test.cs", "namespace Test { public class Test { } }");
+
+            // generate sources
+        }
+    }
+}
