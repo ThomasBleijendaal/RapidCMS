e006a3d40c21cbdc2d33160441e5e0909f6aebe6
Thomas Bleijendaal <t.bleijendaal@wearetriple.com>
2022-01-01 15:14:25 +0100

    Better page state

---
+++ src/RapidCMS.Core/Resolvers/Setup/CollectionListSetupResolverBase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using RapidCMS.Core.Abstractions.Resolvers;
+using RapidCMS.Core.Abstractions.Setup;
+using RapidCMS.Core.Enums;
+using RapidCMS.Core.Models.Config;
+
+namespace RapidCMS.Core.Resolvers.Setup
+{
+    internal class CollectionListSetupResolverBase
+    {
+        protected readonly Lazy<ISetupResolver<ICollectionSetup>> _setupResolver;
+
+        public CollectionListSetupResolverBase(Lazy<ISetupResolver<ICollectionSetup>> setupResolver)
+        {
+            _setupResolver = setupResolver;
+        }
+
+        protected async Task<UsageType> GetUsageTypeAsync(CollectionListConfig config, ICollectionSetup? collection)
+        {
+            if (config is ReferencedCollectionListConfig referencedCollectionList)
+            {
+                if (referencedCollectionList.CollectionAlias == collection?.Alias)
+                {
+                    return (collection.ListEditor != null ? UsageType.Edit : 0) |
+                        (collection.ListView != null ? UsageType.View : 0);
+                }
+                else
+                {
+                    // TODO: this can trigger infinite loops / stack overflows..
+
+                    var resolver = _setupResolver.Value;
+
+                    var referencedCollection = await resolver.ResolveSetupAsync(referencedCollectionList.CollectionAlias);
+
+                    return (referencedCollection?.ListEditor != null ? UsageType.Edit : 0) |
+                        (referencedCollection?.ListView != null ? UsageType.View : 0);
+                }
+            }
+            else
+            {
+                return (config?.ListEditor != null ? UsageType.Edit : 0) |
+                    (config?.ListView != null ? UsageType.View : 0);
+            }
+        }
+    }
+}
--- src/RapidCMS.Core/Resolvers/Setup/SubCollectionListSetupResolver.cs
+++ src/RapidCMS.Core/Resolvers/Setup/SubCollectionListSetupResolver.cs
@@ -2,53 +2,20 @@
 using System.Threading.Tasks;
 using RapidCMS.Core.Abstractions.Resolvers;
 using RapidCMS.Core.Abstractions.Setup;
-using RapidCMS.Core.Enums;
 using RapidCMS.Core.Models.Config;
 using RapidCMS.Core.Models.Setup;
 
 namespace RapidCMS.Core.Resolvers.Setup
 {
-    internal class SubCollectionListSetupResolver : ISetupResolver<ISubCollectionListSetup, CollectionListConfig>
+    internal class SubCollectionListSetupResolver : CollectionListSetupResolverBase, ISetupResolver<ISubCollectionListSetup, CollectionListConfig>
     {
-        private readonly Lazy<ISetupResolver<ICollectionSetup>> _setupResolver;
-
-        public SubCollectionListSetupResolver(Lazy<ISetupResolver<ICollectionSetup>> setupResolver)
+        public SubCollectionListSetupResolver(Lazy<ISetupResolver<ICollectionSetup>> setupResolver) : base(setupResolver)
         {
-            _setupResolver = setupResolver;
         }
 
         public async Task<IResolvedSetup<ISubCollectionListSetup>> ResolveSetupAsync(CollectionListConfig config, ICollectionSetup? collection = default)
         {
-            UsageType usageType;
-            
-            if (config is ReferencedCollectionListConfig referencedCollectionList)
-            {
-                if (referencedCollectionList.CollectionAlias == collection?.Alias)
-                {
-                    usageType =
-                        (collection.ListEditor != null ? UsageType.Edit : 0) |
-                        (collection.ListView != null ? UsageType.View : 0);
-                }
-                else
-                {
-                    // TODO: this can trigger infinite loops / stack overflows..
-                    // TODO: check collection subcollections before using the resolver
-
-                    var resolver = _setupResolver.Value;
-
-                    var referencedCollection = await resolver.ResolveSetupAsync(referencedCollectionList.CollectionAlias);
-
-                    usageType =
-                        (referencedCollection?.ListEditor != null ? UsageType.Edit : 0) |
-                        (referencedCollection?.ListView != null ? UsageType.View : 0);
-                }
-            }
-            else
-            {
-                usageType =
-                    (config?.ListEditor != null ? UsageType.Edit : 0) |
-                    (config?.ListView != null ? UsageType.View : 0);
-            }
+            var usageType = await GetUsageTypeAsync(config, collection);
 
             return new ResolvedSetup<ISubCollectionListSetup>(
                 new SubCollectionListSetup(config!.Index, config.CollectionAlias)
