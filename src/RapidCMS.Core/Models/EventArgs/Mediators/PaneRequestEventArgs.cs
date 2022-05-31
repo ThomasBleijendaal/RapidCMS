1caa9c36b73a96efc0b83e8047fda2c7e19ab226
Thomas Bleijendaal <ThomasBleijendaal@users.noreply.github.com>
2022-05-31 20:17:21 +0200

    Minor optimization (#174)

--- src/RapidCMS.Core/Models/EventArgs/Mediators/PaneRequestEventArgs.cs
+++ src/RapidCMS.Core/Models/EventArgs/Mediators/PaneRequestEventArgs.cs
@@ -7,17 +7,17 @@ namespace RapidCMS.Core.Models.EventArgs.Mediators
 {
     public class PaneRequestEventArgs : IMediatorRequestEventArgs<CrudType>
     {
-        public PaneRequestEventArgs(Type paneType, FormEditContext editContext, ButtonContext buttonContext)
+        public PaneRequestEventArgs(Type paneType, FormEditContext? editContext, ButtonContext buttonContext)
         {
             PaneType = paneType;
-            EditContext = editContext ?? throw new ArgumentNullException(nameof(editContext));
+            EditContext = editContext;
             ButtonContext = buttonContext ?? throw new ArgumentNullException(nameof(buttonContext));
         }
 
         public Guid RequestId { get; set; }
 
         public Type PaneType { get; set; }
-        public FormEditContext EditContext { get; set; }
+        public FormEditContext? EditContext { get; set; }
         public ButtonContext ButtonContext { get; set; }
     }
 }
