48a11c154d0ccb0182cce48dd400a4ca3a91ddd0
Thomas Bleijendaal <t.bleijendaal@wearetriple.com>
2022-10-09 15:47:00 +0200

    WIP

---
+++ extensions/RapidCMS.Extensions.Markdown/ConfigExtensions.cs
@@ -0,0 +1,11 @@
+﻿using RapidCMS.Core.Abstractions.Config;
+using RapidCMS.Core.Abstractions.Data;
+
+namespace RapidCMS.Extensions.Markdown
+{
+    public static class ConfigExtensions
+    {
+        public static IEditorFieldConfig<TEntity, TValue> SetAsMarkdownEditor<TEntity, TValue>(this IEditorFieldConfig<TEntity, TValue> field) where TEntity : IEntity
+            => field.SetType(typeof(MarkdownEditor));
+    }
+}
